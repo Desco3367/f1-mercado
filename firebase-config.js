a140c26b65be2c30be8021966e839175e06bf974
Desco3367 <jrvb2003@gmail.com>
2026-06-05 15:23:51 -0300

    Add Firebase config

---
+++ firebase-config.js
@@ -0,0 +1,10 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyDxvAaM84OEeMl2D98ORnOSDb9NQVF9RhQ",
+  authDomain: "pujasf1manager.firebaseapp.com",
+  databaseURL: "https://pujasf1manager-default-rtdb.firebaseio.com",
+  projectId: "pujasf1manager",
+  storageBucket: "pujasf1manager.firebasestorage.app",
+  messagingSenderId: "917821173780",
+  appId: "1:917821173780:web:7f533afe109496fe9ff701",
+  measurementId: "G-LK6JQX4D7M",
+};
